8856fc49132818853425aa0edfaf6957adaec260
alasalamont <113342904+alasalamont@users.noreply.github.com>
2024-03-20 22:08:46 +0700

    Update gen-gopher-ssrf-payload.py

--- gen-gopher-ssrf-payload.py
+++ gen-gopher-ssrf-payload.py
@@ -13,14 +13,20 @@ init(autoreset=True)  # Auto reset color for each print
 import os
 import subprocess
 
-def main_menu():
-    print(f"""
-{Style.BRIGHT}[+] Option-1. {Fore.GREEN}gopher-get{Style.RESET_ALL}{Style.BRIGHT}: access to a specific endpoint/file
-{Style.BRIGHT}[+] Option-2. {Fore.GREEN}gopher-post-url{Style.RESET_ALL}{Style.BRIGHT}: send data to a specific endpoint/file via {Fore.GREEN}URL{Style.RESET_ALL}{Style.BRIGHT}
-{Style.BRIGHT}[+] Option-3. {Fore.GREEN}gopher-post-json{Style.RESET_ALL}{Style.BRIGHT}: send data to a specific endpoint/file, the data-sent is in {Fore.GREEN}json format{Style.RESET_ALL}{Style.BRIGHT}
-{Style.BRIGHT}[+] Option-4. {Fore.GREEN}gopher-post-normal{Style.RESET_ALL}{Style.BRIGHT}: send data to a specific endpoint/file, the data-sent is in {Fore.GREEN}www-form-urlencoded format{Style.RESET_ALL}{Style.BRIGHT}
-
-{Style.BRIGHT}+ Please choose your option:""")
+def run_script(option):
+    # Map the option numbers to their corresponding script filenames
+    scripts = {
+        1: "option-1-gopher-get.py",
+        2: "option-2-gopher-post-url.py",
+        3: "option-3-gopher-post-json.py",
+        4: "option-4-gopher-post-basic.py",
+    }
+    script_name = scripts.get(option)
+    script_path = f"./{script_name}"
+    if os.path.exists(script_path):
+        subprocess.run(["python3", script_path], check=True)
+    else:
+        print(f"Error: The script for option {option} does not exist.")
 
 
 def run_script(option):
