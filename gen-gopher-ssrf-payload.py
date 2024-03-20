c149bd2d7b541bb0d58bee754fcfe76586582e54
alasalamont <113342904+alasalamont@users.noreply.github.com>
2024-03-20 22:17:27 +0700

    Update gen-gopher-ssrf-payload.py

--- gen-gopher-ssrf-payload.py
+++ gen-gopher-ssrf-payload.py
@@ -1,15 +1,27 @@
+#!/usr/bin/python3
+
+#Note
+#This file will give the option for user to choose. It will call 4 files as follow:
+#1. option-1-gopher-get.py
+#2. option-2-gopher-post-url.py
+#3. option-3-gopher-post-json
+#4. option-4-gopher-post-basic`
+#→ Need to put the above 4 files together with this file
+
+from colorama import Fore, Back, Style, init
+init(autoreset=True)  # Auto reset color for each print
 import os
 import subprocess
 
 def main_menu():
-    print("""
-- Option 1. gopher-get: access to a specific endpoint/file
-- Option 2. gopher-post-url: send data to a specific endpoint/file via URL
-- Option 3. gopher-post-json: send data to a specific endpoint/file, the data-sent is in json format
-- Option 4. gopher-post-normal: send data to a specific endpoint/file, the data-sent is in www-form-urlencoded format
+    print(f"""
+{Style.BRIGHT}[+] Option-1. {Fore.GREEN}gopher-get{Style.RESET_ALL}{Style.BRIGHT}: access to a specific endpoint/file
+{Style.BRIGHT}[+] Option-2. {Fore.GREEN}gopher-post-url{Style.RESET_ALL}{Style.BRIGHT}: send data to a specific endpoint/file via {Fore.GREEN}URL{Style.RESET_ALL}{Style.BRIGHT}
+{Style.BRIGHT}[+] Option-3. {Fore.GREEN}gopher-post-json{Style.RESET_ALL}{Style.BRIGHT}: send data to a specific endpoint/file, the data-sent is in {Fore.GREEN}json format{Style.RESET_ALL}{Style.BRIGHT}
+{Style.BRIGHT}[+] Option-4. {Fore.GREEN}gopher-post-normal{Style.RESET_ALL}{Style.BRIGHT}: send data to a specific endpoint/file, the data-sent is in {Fore.GREEN}www-form-urlencoded format{Style.RESET_ALL}{Style.BRIGHT}
 
-+ Please choose your option:
-""")
+{Style.BRIGHT}[+] Please choose your option:""")
+
 
 def run_script(option):
     # Map the option numbers to their corresponding script filenames
@@ -29,7 +41,7 @@ def run_script(option):
 if __name__ == "__main__":
     main_menu()
     try:
-        choice = int(input("Your choice (1-4): "))
+        choice = int(input(f"{Style.BRIGHT}[→] Your choice (1-4): "))
         if choice in range(1, 5):
             run_script(choice)
         else:
